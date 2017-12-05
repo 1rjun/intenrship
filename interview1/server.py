7d4373d1804e8c0ef7e12da92cb55d60b0311d0f
1rjun <arjunsumbria@gmail.com>
2017-12-05 17:34:41 +0530

    i finally connected it with the database

--- interview1/server.py
+++ interview1/server.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask ,render_template, request,redirect
 
 #this will connect my postgres database test with python3
 import psycopg2
 
 #create the flask app
-app = Flask(__app__)
+app = Flask(__name__)
 
 #creating the string which contain our connection info
 conn_string = "host='localhost' dbname='test' user='postgres' password='arjunalone18'"
@@ -14,10 +14,30 @@ conn = psycopg2.connect(conn_string)
 
 
 #a get request
-@app.route('/',methods=["GET"])
+@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/post_location',methods=["POST"])
+def post_location():
+    pincode = str(request.form["pincode"])
+    address = str(request.form["address"])
+    city = str(request.form["city"])
+    latitude = float(request.form["latitude"])
+    longitude = float(request.form["longitude"])
+    accuracy = int(request.form["accuracy"])
+    curr = conn.cursor()
+    try:
+        curr.execute("""
+        INSERT INTO INDIA VALUES(
+            '{}','{}','{}','{}','{}','{}'
+        );
+        """.format(pincode,address,city,latitude,longitude,accuracy))
+        return "Your data has been submitted"
+    except:
+        return "Sorry primary key already matched"
+        
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
